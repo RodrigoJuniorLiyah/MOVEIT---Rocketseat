6f15105ca91cbde780776c261a2dd785360690d3
Rodrigo Junior <rodrigo.junior7@outlook.com>
2021-02-28 15:20:53 -0300

    correção

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
-import styles from '../styles/components/completedChallenges.module.css';
+import styles from '../styles/Components/completedChallenges.module.css';
 
 export function CompletedChallenges() {
   const { challengesCompleted } = useContext(ChallengesContext);
